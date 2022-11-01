ff1f2051c8147aaebf62d66b7c774d206c0a033a
Eli Qian <eliqian@elis-macbook-air.local>
2022-11-01 03:51:13 +0000

    .

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-function returnText() {
-  var issn = document.getElementById('input_id').value;
-  // var date = document.getElementById('date_id').value;
-  //document.getElementById('alert').innerHTML = 'The ISSN is ' + issn
-  // location.href = "/" + issn + "/" + "date";
-}
